c986c7fbcc69b6a6bd9c52faf9dc8a81ea1a35a1
PAUL <you@example.com>
2023-02-05 14:31:21 +0300

    adding a read Me

---
+++ README.md
@@ -0,0 +1,17 @@
+Clone this app or download it as a ZIP FIlE
+
+Start the virtual environment
+  - virtualenv venv
+  - source venv/Scripts/activate
+
+
+Install requirements as
+    pip install -r requirements.txt
+
+Run the server
+    python manage.py runserver
+
+In 2 different pages and type LOBBY in the chat room
+
+in the chatroom, you can add text freely and the messages will appear.
+
--- mysite/settings.py
+++ mysite/settings.py
@@ -76,12 +76,14 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+        "TEST": {
+            "NAME": BASE_DIR / "db.sqlite3",
+        },
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
